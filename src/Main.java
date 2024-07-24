cdf37a6e508e01fc2b81bd5e22de6fd7de650adb
rgustavo03 <rgustavo032001@gmail.com>
2024-07-24 19:33:47 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso de react");
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(mentoria1);
+    }
+}
